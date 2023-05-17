367beac13c2a299137017ca49e450751b3029fb6
ehmaddd <ehmaddd@gmail.com>
2023-05-17 23:53:25 +0500

    Add changes

--- src/component/Rockets.jsx
+++ src/component/Rockets.jsx
@@ -1,5 +1,30 @@
-const Rockets = () => (
-  <h1>Rockets</h1>
-);
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import showRocket from './showRocket';
+import { getRockets } from '../Redux/rockets/rocketSlice';
+
+const Rockets = () => {
+  const { rockets, isLoading } = useSelector((state) => state.rockets);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div>{showRocket(rockets)}</div>
+    </div>
+  );
+};
 
 export default Rockets;
