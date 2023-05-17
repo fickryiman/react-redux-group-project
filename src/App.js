1dbbec04ed9b9287a5d831f2393524becb28e3ae
ehmaddd <ehmaddd@gmail.com>
2023-05-17 14:57:05 +0500

    Add switch badge files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './component/Navbar';
-import { getMissions } from './Redux/missions/missionSlice';
 import './App.css';
 
 function App() {
-  const { isLoading } = useSelector((state) => state.missions);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getMissions());
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
   return (
     <BrowserRouter>
       <Navbar />
