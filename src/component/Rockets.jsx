3a75aa437450df9b213dffc4470eea61bbb5bf12
Fickry Bil Iman <fickry.iman@gmail.com>
2023-05-18 23:47:13 +0700

    Revert "Rocket booking actions"

--- src/component/Rockets.jsx
+++ src/component/Rockets.jsx
@@ -1,9 +1,17 @@
-import { useSelector } from 'react-redux';
-import ShowRocket from './showRocket';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import showRocket from './showRocket';
+import { getRockets } from '../Redux/rockets/rocketSlice';
 import './Rockets.css';
 
 const Rockets = () => {
   const { rockets, isLoading } = useSelector((state) => state.rockets);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
 
   if (isLoading) {
     return <div>Loading contents...</div>;
@@ -11,13 +19,9 @@ const Rockets = () => {
 
   return (
     <div className="rockets-container">
+      {/* <div>{showRocket(rockets)}</div> */}
       <div className="cards">
-        {rockets.map((rocket) => ShowRocket(
-          rocket.rocketId,
-          rocket.rocketName,
-          rocket.description,
-          rocket.flickrImages,
-        ))}
+        {rockets.map((rocket) => showRocket(rocket))}
       </div>
     </div>
   );
