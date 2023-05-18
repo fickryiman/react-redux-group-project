c796a3d7473163e013f30ed9972cd6192b06e9fa
Fickry Iman <fickry.iman@gmail.com>
2023-05-18 19:45:12 +0700

    Following features are applied :
    - Followed the same logic as with the "reserve rocket" - but set the reserved key to false.
    - Dispatched the actions upon click on the corresponding buttons.

--- src/component/showRocket.jsx
+++ src/component/showRocket.jsx
@@ -14,6 +14,7 @@ const ShowRocket = (rocketId, rocketName, description, flickrImages) => {
         <button
           type="button"
           className="reserve-rocket"
+          id={rocketId}
           onClick={() => dispatch(reserveRocket(rocketId))}
         >
           Reserve Rocket
