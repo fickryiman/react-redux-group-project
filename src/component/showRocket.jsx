22f74a6b6d2e7c1a9e09f40ad87c6791d8835929
Fickry Bil Iman <fickry.iman@gmail.com>
2023-05-18 23:46:57 +0700

    Revert "Rocket booking cancelations actions"

--- src/component/showRocket.jsx
+++ src/component/showRocket.jsx
@@ -14,7 +14,6 @@ const ShowRocket = (rocketId, rocketName, description, flickrImages) => {
         <button
           type="button"
           className="reserve-rocket"
-          id={rocketId}
           onClick={() => dispatch(reserveRocket(rocketId))}
         >
           Reserve Rocket
