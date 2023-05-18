01008e1212af181a89ec6358dae5a87d6b044fb8
Fickry Bil Iman <fickry.iman@gmail.com>
2023-05-18 23:46:37 +0700

    Revert "add badges after clicked reserve rocket button"

--- src/component/showRocket.jsx
+++ src/component/showRocket.jsx
@@ -2,7 +2,7 @@ import { useDispatch } from 'react-redux';
 import { reserveRocket } from '../Redux/rockets/rocketSlice';
 import './showRocket.css';
 
-const ShowRocket = (rocketId, rocketName, description, flickrImages, reserved) => {
+const ShowRocket = (rocketId, rocketName, description, flickrImages) => {
   const dispatch = useDispatch();
 
   return (
@@ -10,37 +10,15 @@ const ShowRocket = (rocketId, rocketName, description, flickrImages, reserved) =
       <img src={flickrImages[0]} className="flickr-images" alt="flickr-images" />
       <div className="name-desc-button">
         <h3 className="rocket-name">{rocketName}</h3>
-        {
-          !reserved ? (
-            <p className="description">{ description }</p>
-          ) : (
-            <p className="description">
-              <span className="badges">Reserved</span>
-              { description }
-            </p>
-          )
-        }
-        {
-          !reserved ? (
-            <button
-              type="button"
-              className="reserve-rocket"
-              id={rocketId}
-              onClick={() => dispatch(reserveRocket(rocketId))}
-            >
-              Reserve Rocket
-            </button>
-          ) : (
-            <button
-              type="button"
-              className="cancel-reservation"
-              id={rocketId}
-              onClick={() => dispatch(reserveRocket(rocketId))}
-            >
-              Cancel Reservation
-            </button>
-          )
-        }
+        <p className="description">{description}</p>
+        <button
+          type="button"
+          className="reserve-rocket"
+          id={rocketId}
+          onClick={() => dispatch(reserveRocket(rocketId))}
+        >
+          Reserve Rocket
+        </button>
       </div>
     </div>
   );
