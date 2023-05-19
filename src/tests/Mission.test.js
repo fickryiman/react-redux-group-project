986042bd161037bc1172280ddd71ca06c4c967d7
Fickry Iman <fickry.iman@gmail.com>
2023-05-19 20:13:56 +0700

    fixes typo

--- src/tests/Mission.test.js
+++ src/tests/Mission.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import Missions from '../components/Missions';
-import store from '../redux/store';
+import store from '../Redux/store';
 
 test('It should render correctly', () => {
   const tree = render(
